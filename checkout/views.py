734e12983e0be4aae20bb8a0a2e444757571e8c9
Cutbert1 <cutbertogbozor@yahoo.co.uk>
2025-04-04 16:02:12 +0100

    Add Strip elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51RAA2e4Gn0n89g1X6vHcxhQysQAWHOzQSMHPKoIGBZcYFIwq9Yy09XtmMl2FOL4dWLCm2MISYDOnH9bpFHelqGlS00TrFqdreF',  # noqa
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
